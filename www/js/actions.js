b7e8bbb3ebc810a5bd3f8b299a59d09a56fed4f1
John McLear <john@mclear.co.uk>
2014-01-02 16:28:30 +0000

    first commit

---
+++ www/js/actions.js
@@ -0,0 +1,61 @@
+var actions = {
+  twitter: {
+    label: "Twitter",
+    optionText: "What is your Twitter Username?",
+    placeHolder: "@nfcring",
+    prefix: "http://twitter.com/",
+    format: function (option) {
+      return this.prefix + option
+    }
+  },
+  facebook: {
+    label: "Facebook",
+    optionText: "What is your Facebook Page URL?",
+    placeHolder: "http://facebook.com/ring.cake24",
+    prefix: "http://facebook.com/",
+    format: function (option) {
+      return this.prefix + option
+    }
+  },
+  /*
+  clone: {
+    label: "Clone Ring",
+    isClone: true,
+    requiresString: false
+  },
+  */
+  website: {
+    label: "Website",
+    optionText: "What is the URL of the website?",
+    placeHolder: "http://mclear.co.uk",
+    format: function (option) {
+      return option
+    }
+  },
+  /*
+  ,
+  skype: {
+    label: "Skype",
+    optionText: "What is your Skype Username?",
+    placeHolder: "JohnMcLear"
+  }
+  */
+  etherpad: {
+    label: "Etherpad",
+    optionText: "What is your Pad URL?",
+    placeHolder: "http://beta.etherpad.org/p/foowie",
+    format: function (option) {
+      return option
+    }
+  },
+  youtube: {
+    label: "Youtube",
+    optionText: "What is your Youtube Video / Channel?",
+    placeHolder: "johnyma22",
+    prefix: "http://youtube.com",
+    format: function (option) {
+      return this.prefix + option
+    }
+  }
+};
+
