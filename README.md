f928d90cf8e31d91aaab1536322b9ea31836cc7e
John McLear <john@mclear.co.uk>
2014-01-03 16:45:55 +0000

    Masses of fixes also move from phonegap to cordova cause phonegap build is too buggy

---
+++ README.md
@@ -0,0 +1,6 @@
+cordova create NFCRingPro co.mclear.nfcringpro NFCRingPro
+cordova platform add android
+cordova plugin add https://github.com/chariotsolutions/phonegap-nfc.git
+cordova plugin add https://github.com/apache/cordova-plugin-device.git
+cordova plugin add org.apache.cordova.dialogs
+cordova plugin add org.apache.cordova.vibration
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/jose/NFCRingPro/platforms/android/gen/co/mclear/nfcringpro/R.java \
+ : /home/jose/NFCRingPro/platforms/android/res/values/strings.xml \
+/home/jose/NFCRingPro/platforms/android/res/drawable/icon.png \
+/home/jose/NFCRingPro/platforms/android/res/drawable-xhdpi/icon.png \
+/home/jose/NFCRingPro/platforms/android/res/xml/config.xml \
+/home/jose/NFCRingPro/platforms/android/res/drawable-ldpi/icon.png \
+/home/jose/NFCRingPro/platforms/android/res/drawable-hdpi/icon.png \
+/home/jose/NFCRingPro/platforms/android/res/drawable-mdpi/icon.png \
+/home/jose/NFCRingPro/platforms/android/bin/AndroidManifest.xml \
